8b5702c4d4537755ea6c684ec695fe534bc24b4f
Tippy Cows <60576525+Code10M82@users.noreply.github.com>
2020-07-24 18:10:53 -0700

    edit fields working correctly now

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-console.log('1234');
+console.log("I've refreshed");
+
 const editButton = document.querySelector('.button__edit');
 const closeButton = document.querySelector('.button__close');
-const modal = document.querySelector('.modal');
+const popup = document.querySelector('.popup');
 const saveButton = document.querySelector('.button__save');
+const form = document.querySelector('.popup__form');
+const nameValue = document.querySelector('.popup__value_name');
+const titleValue = document.querySelector('.popup__value_title');
+const nameField = document.querySelector('.profile__name');
+const titleField = document.querySelector('.profile__title');
 
-
-function toggleModal() {
-  modal.classList.toggle('modal__open');
+function togglePopup() {
+  popup.classList.toggle('popup_open');
 }
+//  function e() {
+//    e.preventDefault();
+//  }
 
-editButton.addEventListener('click', toggleModal);
-
-closeButton.addEventListener('click', toggleModal);
-console.log('grrr');
-saveButton.addEventListener('submit', function(e) {
+editButton.addEventListener('click', togglePopup);
   
+
+closeButton.addEventListener('click', togglePopup);
+
+form.addEventListener('submit', function(e) {
   e.preventDefault();
-  console.log(e);
-  
-  
 
-  
+  nameField.textContent = nameValue.value;
+  titleField.textContent = titleValue.value;
 });
 
+saveButton.addEventListener('click', togglePopup);
+
+
+console.log('does this work yet,');
